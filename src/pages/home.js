70c6d1ac9c3b92864ecb2cda8183ddbd42cc8f57
Dogl4 <doougllas@hotmail.com.br>
2023-01-14 03:33:27 -0300

    build: 🚧 init project with `vercel`

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavBar } from '../components/NavBar';
+
+const home = () => {
+  return (
+    <>
+      <NavBar />
+      <div>home</div>
+    </>
+  )
+}
+
+export default home;
